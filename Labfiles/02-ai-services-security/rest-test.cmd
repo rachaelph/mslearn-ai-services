ce82e7f1c552d13fee9a70a79ca745cbd0b79cc5
Rachael Phillips <rachael.aphillips@outlook.com>
2024-10-15 15:45:11 -0400

    saved code

--- Labfiles/02-ai-services-security/rest-test.cmd
+++ Labfiles/02-ai-services-security/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "<your-endpoint>/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <your-key>" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
+curl -X POST "https://azureairap.cognitiveservices.azure.com/language/:analyze-text?api-version=2023-04-01" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: eb7cb6144a434e24a0a37009409d681a" --data-ascii "{'analysisInput':{'documents':[{'id':1,'text':'hello'}]}, 'kind': 'LanguageDetection'}"
